47442fa9155814e7e5abdf855f92e53c515e441f
Shelton Gayles <sgbythelake@gmail.com>
2018-09-17 07:55:50 -0700

    where i stopped

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //$this->call(UsersTableSeeder::class);
-        $this->call(TodosTableSeeder::class);
-
+        // $this->call(TodosTableSeeder::class);
+        #Order of seeder is important
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
